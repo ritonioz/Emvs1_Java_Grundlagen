d78416e75bcfdb9402fd3f70f5f323bcb36c8a59
ritonioz <ritonioz911@gmail.com>
2025-10-01 08:41:46 +0200

    00_A_AsciiAusgabe

---
+++ 00_A_AsciiAusgabe/00_A_AsciiAusgabe/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+         Try to output "JAVA" with Ascii-art.
+         Use the "System.out.println" command.
+         Print your result on the console.
+         The first letter "J" is already done as an example.
+        */
+
+        System.out.println("*******       *  *           *  *");
+        System.out.println("      *      * *  *         *  * *");
+        System.out.println("      *     *   *  *       *  *   *");
+        System.out.println("      *    *******  *     *  *******");
+        System.out.println("*     *   *       *  *   *  *       *");
+        System.out.println("*     *  *         *  * *  *         *");
+        System.out.println("******* *           *  *  *           *");
+
+
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        // Example for the letter "J":
+        //    *******
+        //          *
+        //          *
+        //          *
+        //    *     *
+        //    *     *
+        //    *******
+
+        System.out.println("      *        **      *  *********    * * * * *     **      *  *     * * * * *");
+        System.out.println("     * *       * *     *      *       *         *    * *     *  *    *         *");
+        System.out.println("    *   *      *  *    *      *      *           *   *  *    *  *   *           *");
+        System.out.println("   * * * *     *   *   *      *     *             *  *   *   *  *  *             *");
+        System.out.println("  *       *    *    *  *      *      *           *   *    *  *  *   *           *");
+        System.out.println(" *         *   *     * *      *       *         *    *     * *  *    *         *");
+        System.out.println("*           *  *      **      *        * * * * *     *      **  *     * * * * *");
+
+        // After you successfully printed "JAVA" as Ascii-art on the console, try
+        // to implement your own ascii art!
+        // It can be your name, your hobby, your favorite food or even some fancy art!
+        // Feel free to get inspiration on the internet!
+
+        // Of course, also print it out on the console.
+    }
+}
