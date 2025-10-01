db995b67d15a9bec2dd7afc015622ba48ad3f210
ritonioz <ritonioz911@gmail.com>
2025-10-01 08:43:00 +0200

    01_A_Kommentare

---
+++ 01_A_Kommentare/01_A_Kommentare/src/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // This is a one-line comment
+        // Everything after the '//' is ignored until the end of the line................even here!
+        // The color of comments are usually in one color, distinct of 'code'-color
+        // See the next line? Actually code will have different colors (Colors vary depending on the used IDE / Theme)
+        System.out.println("This will still be printed, try it out!"); // Also works here!
+
+        // Again: Everything after the '//' will be completely ignored
+        // Even this code:  System.out.println("This will NOT be printed");
+
+        /*
+            And this
+            is a comment
+            spanning multiple lines!
+            Everything within the opening
+            and closing symbols is ignored.
+            Pay attention to the opening and closing symbols: It starts with as slash, followed by an asterisk,
+            but ends with an asterisk, then a slash
+        */
+
+
+        //--------------------------------------------------------------------------------------------------------------
+        // Sometimes comments like this here is used to mark a section.
+        // If doing so is 'clean code' or not is a subject of it's own :)
+        // -------------------------------------------------------------------------------------------------------------
+
+        // Coding can be hard,
+        // but also fun
+        // and rewarding!
+
+        // Try to comment out the next few lines so, that the result of the console will be as the comments above
+        //System.out.print("Coding can be");
+        System.out.print("Coding is definitely");
+        System.out.println(" hard,");
+        /*System.out.print(" wild,");
+        System.out.println("but also rewarding!");
+        */System.out.println("but also fun");
+        //System.out.println("and rewarding!");
+
+        // What is the difference between "println" and "print"?
+        // Answer:
+
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        // Comment the section below out like described in the "System.out.println(...)"-itself
+
+        //System.out.println("Comment these lines");
+        //System.out.println("out with");
+        //System.out.println("single-line");
+        //System.out.println("comments.");
+
+
+        /*System.out.println("And comment");
+        System.out.println("these lines");
+        System.out.println("with");
+        System.out.println("multiple lines");
+        System.out.println("comments out.");
+        */
+    }
+}
