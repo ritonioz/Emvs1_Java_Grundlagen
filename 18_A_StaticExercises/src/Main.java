07b05298c3e111d70fd863d8f013793d844d67e1
ritonioz <ritonioz911@gmail.com>
2026-01-07 19:58:06 +0100

    18_A_Arrays

---
+++ 18_A_StaticExercises/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println();//because main and staticExcercise isn't together
+        System.out.println(Math.PI);
+        //math.E euler Zahl
+         //math.TAU double of PI
+         //math.DEGREES_TO_RADIANS idk
+        String test02 = "Salü Wält!";
+        Utils.printInput(test02); // we don't have to create every time an object
+
+        String test03 = "2.35f";
+        Utils.printInput(test03);
+        String test04 = "123.8175419823d";
+        Utils.printInput(test04);
+        String test05 = "true";
+        Utils.printInput(test05);
+        String test06 = "false";
+        Utils.printInput(test06);
+        String test07 = "a";
+        Utils.printInput(test07);
+        String test08 = "(byte)32)";
+        Utils.printInput(test08);
+        String test09 = "-5000";
+        Utils.printInput(test09);
+
+
+    }
+}
---
+++ 18_A_StaticExercises/src/Utils.java
@@ -0,0 +1,5 @@
+public class Utils {
+    public static void printInput(String input){
+        System.out.println(input);
+    }
+}
